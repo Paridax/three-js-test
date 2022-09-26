f28debf4f1a750b33f8f9e7138edb1384602daad
Paridax <tgavdaly@gmail.com>
2022-09-26 11:37:55 -0400

    Add mouse input detection

--- src/client/utils/InputManager.ts
+++ src/client/utils/InputManager.ts
@@ -9,9 +9,29 @@ export class InputManager {
       // remove item from map
       this.keysPressed.delete(e.key.toLowerCase())
     })
+
+    window.addEventListener('mousedown', (e) => {
+      this.keysPressed.set(e.button, true)
+    })
+
+    window.addEventListener('mouseup', (e) => {
+      this.keysPressed.delete(e.button)
+    })
   }
   
   public get(key: string) {
     return this.keysPressed.get(key)
+  }
+
+  public getMouse(button: 'left' | 'right' | 'middle' = 'left') {
+    // console.log(this.keysPressed)
+    switch(button) {
+      case 'left':
+        return this.keysPressed.get(0)
+      case 'right':
+        return this.keysPressed.get(2)
+      case 'middle':
+        return this.keysPressed.get(1)
+    }
   }
 }
