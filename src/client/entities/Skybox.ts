f4d29b438ae3e54a1f8fe7507e116a912376ce3e
Paridax <tgavdaly@gmail.com>
2022-09-26 11:37:23 -0400

    Add skybox

---
+++ src/client/entities/Skybox.ts
@@ -0,0 +1,35 @@
+import * as THREE from 'three'
+export class Skybox {
+  private skybox: THREE.BoxGeometry
+  private skyboxMaterials = {
+    top: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/skybox/top.png'), side: THREE.DoubleSide }),
+    bottom: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/skybox/bottom.png'), side: THREE.DoubleSide }),
+    back: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/skybox/left.png'), side: THREE.DoubleSide }),
+    front: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/skybox/right.png'), side: THREE.DoubleSide }),
+    left: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/skybox/front.png'), side: THREE.DoubleSide }),
+    right: new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/skybox/back.png'), side: THREE.DoubleSide }),
+  }
+  private ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+  private sunlight = new THREE.PointLight(0xffffff, 1, 50)
+  private skyMesh: THREE.Mesh
+  
+  constructor(scene: THREE.Scene) {
+    this.skybox = new THREE.BoxGeometry(10000, 10000, 10000)
+    this.skyMesh = new THREE.Mesh(this.skybox, [
+      this.skyboxMaterials.front,
+      this.skyboxMaterials.back,
+      this.skyboxMaterials.top,
+      this.skyboxMaterials.bottom,
+      this.skyboxMaterials.left,
+      this.skyboxMaterials.right,
+    ])
+    
+    this.skyMesh.position.set(0, 0, 0)
+    scene.add(this.skyMesh)
+    scene.add(this.ambientLight)
+  }
+
+  public update(position: THREE.Vector3) {
+    this.skyMesh.position.set(position.x, position.y, position.z)
+  }
+}
